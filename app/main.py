7ca98f23a615814e877ce6ba2c87bf280b76cf13
Naruki-Ichihara <70839257+Naruki-Ichihara@users.noreply.github.com>
2023-01-08 16:59:18 +0900

    added create_server

--- app/main.py
+++ app/main.py
@@ -1,12 +1,18 @@
 # Uncomment this to pass the first stage
 import socket
 
+def create_server(port, ip="localhost"):
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind((ip, port))
+    server.listen()
+    return server
 
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!")
 
-    server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
+    #server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
+    server_socket = create_server(6379)
     client_connection, _ = server_socket.accept() # wait for client
     client_connection.recv(1024)
     client_connection.send(b"+PONG\r\n")
