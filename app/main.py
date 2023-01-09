71472928d93e8f29ada983d7e798ba9a509f898e
Naruki-Ichihara <70839257+Naruki-Ichihara@users.noreply.github.com>
2023-01-09 23:21:05 +0900

    <your_commit_message>

--- app/main.py
+++ app/main.py
@@ -1,21 +1,21 @@
 # Uncomment this to pass the first stage
 import socket
 
-def create_server(port, ip="localhost"):
-    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server.bind((ip, port))
-    server.listen()
+def create_server(port, ip="127.0.0.1"):
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server:
+        server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        server.bind((ip, port))
+        server.listen()
     return server
 
 def main():
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
-    print("Logs from your program will appear here!")
-
-    server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    #server_socket = create_server(6379)
+    #server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
+    server_socket = create_server(6379)
     client_connection, _ = server_socket.accept() # wait for client
-    client_connection.recv(1024)
-    client_connection.send(b"+PONG\r\n")
+    while True:
+        client_connection.recv(1024)
+        client_connection.send(b"+PONG\r\n")
 
 if __name__ == "__main__":
     main()
